2c0c8fa9a04611ca8ba3ed2b6eefc8f990ecb89b
manishu2005 <142162468+manishu2005@users.noreply.github.com>
2025-11-01 18:00:53 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navigation from '../components/Navigation.jsx'
 import Ridepage from '../components/RidePage.jsx'
 import Body from '../components/Body.jsx'
-import Download from '../components/download.jsx'
+import Download from '../components/Download.jsx'
 
 
 function Home() {
